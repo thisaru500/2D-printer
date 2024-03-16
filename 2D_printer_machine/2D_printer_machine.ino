9bea36c262b06c4b542ee5891ff6720963118b3d
thisaru500 <thisarudissanayake123@gmail.com>
2024-03-16 12:59:36 +0530

    2nd day
    Today I created drawing settings , Drawing robot limits and set to true to get debug output

--- 2D_printer_machine/2D_printer_machine.ino
+++ 2D_printer_machine/2D_printer_machine.ino
@@ -21,3 +21,39 @@ Servo  penServo;
 // Identifying X- and Y-axis
 AF_Stepper myStepperY(stepsPerRevolution,1);            
 AF_Stepper  myStepperX(stepsPerRevolution,2);
+
+/* Structures, global variables    */
+struct  point { 
+  float x; 
+  float y; 
+  float z; 
+};
+
+// Current position  of plothead
+struct point actuatorPos;
+
+//  Drawing settings, should be  OK
+float StepInc = 1;
+int StepDelay = 0;
+int LineDelay =0;
+int penDelay  = 50;
+ 
+// Calculate steps per mm. Enter here.
+float  StepsPerMillimeterX = 100.0;
+float StepsPerMillimeterY = 100.0;
+
+// Drawing  robot limits, in mm
+// OK to start with. Could go up to 50 mm if calibrated well.  
+float Xmin = 0;
+float Xmax = 40;
+float Ymin = 0;
+float Ymax = 40;
+float  Zmin = 0;
+float Zmax = 1;
+
+float Xpos = Xmin;
+float Ypos = Ymin;
+float  Zpos = Zmax; 
+
+// Set to true to get debug output.
+boolean verbose = false;
